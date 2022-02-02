da24e5a399d881a01da201cf11e5404f464d23fe
LothaireE <lothaire.epee@gmail.com>
2022-02-02 15:14:41 +0100

    my-counter correct

---
+++ destruc.js
@@ -0,0 +1,10 @@
+const array = ['Bastien', 27]
+
+
+//old way
+// const name = arr[0]
+// const age = arr[1]
+
+//new way
+
+const [name, age] = array
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <span>Counter : {counter}</span>
+
+      {counter < 10 && (
+        <button
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+      )}
+
+      {counter > 0 && (
+        <button
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          -
+        </button>
+      )}
+
+      <button
+        onClick={() => {
+          setCounter(counter * 0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
