d98ab00b3cc936bacbde854e0d2de5461c20034f
LothaireE <lothaire.epee@gmail.com>
2022-02-04 15:29:23 +0100

    push ?

--- my-react-counter-v2/src/App.js
+++ my-react-counter-v2/src/App.js
@@ -3,6 +3,12 @@ import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState([0]);
+  const [addCounter, setAddCounter] = useState(0);
+
+  const handleAddCounterBtn = () => {
+    setAddCounter(addCounter + 1);
+    console.log(addCounter);
+  };
 
   //ma fonction(handlePlusClick) que j'appelle? à laquelle je fais reference? avec onClick
   const handlePlusClick = () => {
@@ -11,10 +17,11 @@ function App() {
 
     //modifier la copie
     newArr.push(counter + 1);
+
     //MAJ l'etat de la copie ? avec la copie ?
     setCounter(newArr);
-    console.log("counter ====>", counter);
-    console.log("newArr ====>", newArr);
+
+    console.log("newArr ====>", counter);
   };
   const handleMinusClick = () => {
     setCounter(counter - 1);
@@ -26,20 +33,76 @@ function App() {
   //  key={index} on verra plus tard
 
   return (
-    <div className="myCounterOne">
-      <div>
-        {counter.map((elem, index) => {
-          return <p> {elem} </p>;
-        })}{" "}
-      </div>
-      <div className="myPlusBtn">
-        <button onClick={handlePlusClick}>+</button>
-      </div>
-      <div className="myMinusBtn">
-        <button onClick={handleMinusClick}>-</button>
-      </div>
-      <div className="myResetBtn">
-        <button onClick={handleResetBtn}>Reset</button>
+    <div className="myContainer">
+      {addCounter < 2 && (
+        <button className="myAddCounterBtn" onClick={handleAddCounterBtn}>
+          Add Counter
+        </button>
+      )}
+
+      <div className="myCounters">
+        <div className="Counter">
+          <div>
+            {counter.map((elem, index) => {
+              console.log("elem ===>", elem);
+              return <p key={index}> {elem} </p>;
+            })}
+          </div>
+
+          <div className="myPlusBtn">
+            <button onClick={handlePlusClick}>+</button>
+          </div>
+          <div className="myMinusBtn">
+            <button onClick={handleMinusClick}>-</button>
+          </div>
+          <div className="myResetBtn">
+            <button onClick={handleResetBtn}>Reset</button>
+          </div>
+        </div>
+        {addCounter >= 1 && (
+          <div className="myCounters">
+            <div className="Counter">
+              <div>
+                {counter.map((elem, index) => {
+                  console.log("elem ===>", elem);
+                  return <p key={index}> {elem} </p>;
+                })}
+              </div>
+
+              <div className="myPlusBtn">
+                <button onClick={handlePlusClick}>+</button>
+              </div>
+              <div className="myMinusBtn">
+                <button onClick={handleMinusClick}>-</button>
+              </div>
+              <div className="myResetBtn">
+                <button onClick={handleResetBtn}>Reset</button>
+              </div>
+            </div>
+          </div>
+        )}
+        {addCounter >= 2 && (
+          <div className="myCounters">
+            <div className="Counter">
+              <div>
+                {counter.map((elem, index) => {
+                  console.log("elem ===>", elem);
+                  return <p key={index}> {elem} </p>;
+                })}
+              </div>
+
+              <div className="myPlusBtn">
+                <button onClick={handlePlusClick}>+</button>
+              </div>
+              <div className="myMinusBtn">
+                <button onClick={handleMinusClick}>-</button>
+              </div>
+              <div className="myResetBtn">
+                <button onClick={handleResetBtn}>Reset</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
