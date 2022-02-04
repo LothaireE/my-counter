581986b3cf6e4da1a9d75e0e298c254c58cf1c0b
LothaireE <lothaire.epee@gmail.com>
2022-02-04 14:01:09 +0100

    line 33 : who is you?

--- my-react-counter-v2/src/App.js
+++ my-react-counter-v2/src/App.js
@@ -2,11 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState([0]);
 
   //ma fonction(handlePlusClick) que j'appelle? à laquelle je fais reference? avec onClick
   const handlePlusClick = () => {
-    setCounter(counter + 1);
+    //copie de counter
+    const newArr = [...counter];
+
+    //modifier la copie
+    newArr.push(counter + 1);
+    //MAJ l'etat de la copie ? avec la copie ?
+    setCounter(newArr);
+    console.log("counter ====>", counter);
+    console.log("newArr ====>", newArr);
   };
   const handleMinusClick = () => {
     setCounter(counter - 1);
@@ -15,9 +23,15 @@ function App() {
     setCounter(0);
   };
 
+  //  key={index} on verra plus tard
+
   return (
-    <div className="myCounter">
-      <p> {counter} </p>
+    <div className="myCounterOne">
+      <div>
+        {counter.map((elem, index) => {
+          return <p> {elem} </p>;
+        })}{" "}
+      </div>
       <div className="myPlusBtn">
         <button onClick={handlePlusClick}>+</button>
       </div>
