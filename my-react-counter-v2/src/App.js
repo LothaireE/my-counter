e22db52ab0cb6a3b0c6ee06c16d34de0140b1423
LothaireE <lothaire.epee@gmail.com>
2022-02-04 13:12:01 +0100

    counter1 status : ok

--- my-react-counter-v2/src/App.js
+++ my-react-counter-v2/src/App.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  //ma fonction(handlePlusClick) que j'appelle? à laquelle je fais reference? avec onClick
+  const handlePlusClick = () => {
+    setCounter(counter + 1);
+  };
+  const handleMinusClick = () => {
+    setCounter(counter - 1);
+  };
+  const handleResetBtn = () => {
+    setCounter(0);
+  };
+
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    <div className="myCounter">
+      <p> {counter} </p>
+      <div className="myPlusBtn">
+        <button onClick={handlePlusClick}>+</button>
+      </div>
+      <div className="myMinusBtn">
+        <button onClick={handleMinusClick}>-</button>
+      </div>
+      <div className="myResetBtn">
+        <button onClick={handleResetBtn}>Reset</button>
+      </div>
     </div>
   );
 }
