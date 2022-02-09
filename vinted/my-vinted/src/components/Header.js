058bee8c7e5439581961a4d1862c5112c90fe4d4
LothaireE <lothaire.epee@gmail.com>
2022-02-09 15:28:38 +0100

    css on point, kind of

---
+++ vinted/my-vinted/src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="my-header">
+      <h1>Mon Header à venir</h1>
+      <li>
+        <button>s'inscrire</button>
+        <button>se connecter</button>
+        <button>vends tes articles</button>
+      </li>
+    </div>
+  );
+};
+export default Header;
